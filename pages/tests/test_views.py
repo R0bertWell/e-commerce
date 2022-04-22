ecdead84ec3a1bd91655c8bd6f53ea1bd79dcf69
r0bertwell <r0bertwell@hotmail.com>
2022-04-22 15:34:15 -0300

    add products app and product tests

--- pages/tests/test_views.py
+++ pages/tests/test_views.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import resolve, reverse
-from django.test import TestCase
+
+from pytest_django.asserts import assertTemplateUsed
 
 @pytest.fixture
 def home_response(client):
@@ -12,7 +13,7 @@ def about_response(client):
     return client.get(reverse("pages:about"))
 
 
-class TestHomePageView(TestCase):
+class TestHomePageView:
     def test_reverse_resolve(self):
         assert reverse('pages:home') == '/'
         assert resolve('/').view_name == 'pages:home'
@@ -21,10 +22,10 @@ class TestHomePageView(TestCase):
         assert home_response.status_code == 200
 
     def test_template(self, home_response):
-        self.assertTemplateUsed(home_response, 'home.html')
+        assertTemplateUsed(home_response, 'home.html')
 
 
-class TestAboutView(TestCase):
+class TestAboutView:
     def test_reverse_resolve(self):
         assert reverse('pages:about') == '/about/'
         assert resolve('/about/').view_name == 'pages:about'
@@ -33,4 +34,4 @@ class TestAboutView(TestCase):
         assert about_response.status_code == 200
 
     def test_template(self, about_response):
-        self.assertTemplateUsed(about_response, 'about.html')
+        assertTemplateUsed(about_response, 'about.html')
